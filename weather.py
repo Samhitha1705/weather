f5aea2ec9cc08ddb45ab1ccb331e94bb0221c40d
samhitha1705 <vedasamhithagopalam@gmail.com>
2026-05-28 09:28:13 +0000

    justt11

--- weather.py
+++ weather.py
@@ -1,28 +1,24 @@
 import requests
+import os
 
 API_KEY = "76b0443d5e257a282e1eaae876528cf7"
 
-CITY = input("Enter city name: ")
+CITY = os.getenv("CITY", "Bangalore")
 
 url = f"https://api.openweathermap.org/data/2.5/weather?q={CITY}&appid={API_KEY}&units=metric"
 
 response = requests.get(url)
 data = response.json()
 
+print("\nWeather Report")
+print("----------------------")
+
 if data["cod"] == 200:
-    print("\nWeather Report")
-    print("----------------------")
     print("City:", data["name"])
     print("Temperature:", data["main"]["temp"], "°C")
     print("Weather:", data["weather"][0]["main"])
     print("Humidity:", data["main"]["humidity"], "%")
-
-    # ✅ ADD THIS HERE (LOGGING PART)
-    with open("weather_log.txt", "a") as f:
-        f.write(f"{data['name']} | {data['main']['temp']}°C | {data['weather'][0]['main']} | {data['main']['humidity']}%\n")
-
 else:
     print("Error:", data["message"])
-
 
 
